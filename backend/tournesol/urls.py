2adc6ec23ea05d53811fb9f74d1e94c8be20e092
Lucas Tarasconi <lucas@tarasconi.fr>
2022-03-10 12:07:35 +0100

    Non connected entities API
    * Show API
    * Some basic test (one not working)
    * The algorithm should be update as not taking all case into account

--- backend/tournesol/urls.py
+++ backend/tournesol/urls.py
@@ -21,6 +21,7 @@ from .views.stats import StatisticsView
 from .views.user import CurrentUserView
 from .views.video import VideoViewSet
 from .views.video_rate_later import VideoRateLaterDetail, VideoRateLaterList
+from .views.unconnected_entities import UnconnectedEntitiesView
 
 router = routers.DefaultRouter()
 router.register(r"video", VideoViewSet, basename="video")
@@ -85,6 +86,12 @@ urlpatterns = [
         "users/me/contributor_ratings/<str:poll_name>/<str:uid>/",
         ContributorRatingDetail.as_view(),
         name="ratings_me_detail",
+    ),
+    # Unconnected entities
+    path(
+        "users/me/unconnected_entities/<str:poll_name>/<str:uid>/",
+        UnconnectedEntitiesView.as_view(),
+        name="unconnected_entities",
     ),
     # Email domain API
     path("domains/", EmailDomainsList.as_view(), name="email_domains_list"),
